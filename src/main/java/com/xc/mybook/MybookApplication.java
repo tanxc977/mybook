02ac54c19e7f20da6f1776fda5411b9491d4d111
tanxc977 <tanxc977@126.com>
2018-01-17 22:01:02 +0800

    Initial commit

---
+++ src/main/java/com/xc/mybook/MybookApplication.java
@@ -0,0 +1,21 @@
+package com.xc.mybook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class MybookApplication {
+
+	@RequestMapping("/")
+	public String home(){
+		return "hello home mybook !";
+	}
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(MybookApplication.class, args);
+	}
+}
