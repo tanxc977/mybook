7b37be333a96c34765367f6ab1ab056545a39c4e
tanxc977 <tanxc977@126.com>
2018-03-12 21:40:25 +0800

    test spring boot jdbc select

--- src/main/java/com/xc/mybook/service/BookService.java
+++ src/main/java/com/xc/mybook/service/BookService.java
@@ -1,14 +1,14 @@
 package com.xc.mybook.service;
 
+import com.xc.mybook.Constants;
+import com.xc.mybook.dao.BookRowMapper;
 import com.xc.mybook.entity.Book;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
 
-import java.sql.ResultSet;
 import java.util.List;
 
 @Service
@@ -19,23 +19,12 @@ public class BookService {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    private final String sql = "select catagory_tag,book_name, enter_date from book where update_date='2018.1.7'";
+    private final String sql = "select seqno, catagory_tag,book_name, enter_date from book where seqno > ? limit 10 ";
 
-    public List<Book> getList(){
-        return  jdbcTemplate.query(sql, new RowMapper<Book>() {
-            @Override
-            public Book mapRow(ResultSet resultSet, int i) {
-                Book book = new Book();
-                try{
-                    book.setCatagoryTag(resultSet.getString("catagory_tag"));
-                    book.setBookName(resultSet.getString("book_name"));
-                    book.setEnterDate(resultSet.getTimestamp("enter_date"));
-                }catch (Exception e){
-                    logger.info("data base map row error" + i);
-                }
-
-                return book;
-            }
-        });
+    private final String sqlInquire = "select seqno,catagory_tag,update_date,book_url,book_name,book_desc,enter_date," +
+            "down_url,down_pwd,image_path,file_path,download_flag from book where seqno > ? limit ?";
+    public List<Book> getList(int pageNo){
+        Integer pageStartId = (int)((pageNo - 1) * Constants.pageNum);
+        return  jdbcTemplate.query(sqlInquire, new Object[]{pageStartId, new Double(Constants.pageNum).intValue()}, new BookRowMapper());
     }
 }
