5245061cc5bee8327fa36861694fa0f4d1a6ae6e
tanxc977 <tanxc977@126.com>
2018-03-26 23:18:25 +0800

    test spring boot jdbc select

--- src/main/java/com/xc/mybook/Constants.java
+++ src/main/java/com/xc/mybook/Constants.java
@@ -2,4 +2,5 @@ package com.xc.mybook;
 
 public class Constants {
     public static final Double pageNum = 4.00;
+    public static final Integer subContentLength = 162;
 }
--- src/main/java/com/xc/mybook/dao/BookDetailRowmapper.java
+++ src/main/java/com/xc/mybook/dao/BookDetailRowmapper.java
@@ -1,5 +1,6 @@
 package com.xc.mybook.dao;
 
+import com.xc.mybook.Constants;
 import com.xc.mybook.entity.BookDetail;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,7 +8,7 @@ import org.springframework.jdbc.core.RowMapper;
 import java.sql.ResultSet;
 
 public class BookDetailRowmapper implements RowMapper {
-    private static final Logger logger = LoggerFactory.getLogger(BookRowMapper.class);
+    private static final Logger logger = LoggerFactory.getLogger(BookDetailRowmapper.class);
 
     @Override
     public BookDetail mapRow(ResultSet resultSet, int i) {
@@ -17,16 +18,27 @@ public class BookDetailRowmapper implements RowMapper {
             bookDetail.setCatagoryTag(resultSet.getString("catagory_tag"));
             bookDetail.setBookName(resultSet.getString("book_name"));
             bookDetail.setEnterDate(resultSet.getTimestamp("enter_date"));
-            bookDetail.setBookDesc(resultSet.getString("book_desc"));
+            String desc = resultSet.getString("book_desc");
+            StringBuilder sbDesc = new StringBuilder();
+            if(desc.length()< Constants.subContentLength){
+                sbDesc.append(desc);
+            }else if(desc.length() >= Constants.subContentLength){
+                sbDesc.append(desc.substring(0,Constants.subContentLength));
+
+            }
+            sbDesc.append("...");
+            bookDetail.setBookDesc(sbDesc.toString());
+
+//            bookDetail.setBookDesc(resultSet.getString("book_desc"));
             bookDetail.setBookUrl(resultSet.getString("book_url"));
             bookDetail.setDownFlag(resultSet.getString("download_flag"));
             bookDetail.setDownUrl(resultSet.getString("down_url"));
             bookDetail.setDownPwd(resultSet.getString("down_pwd"));
             String imagePath = resultSet.getString("image_path");
 
-//            String imageFile = imagePath.substring(imagePath.lastIndexOf("/pic"));
-//            bookDetail.setImagePath(imageFile);
-            bookDetail.setImagePath(imagePath);
+            String imageFile = imagePath.substring(imagePath.lastIndexOf("/pic"));
+            bookDetail.setImagePath(imageFile);
+//            bookDetail.setImagePath(imagePath);
             bookDetail.setFilePath(resultSet.getString("file_path"));
             bookDetail.setUpdateDate(resultSet.getString("update_date"));
             bookDetail.setCatagoryTagMain(resultSet.getString("catagory_tag_main"));
