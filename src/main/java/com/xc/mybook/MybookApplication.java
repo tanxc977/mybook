a29da1748fdf3a4a46ddd3b43af013b42cdd01b5
tanxc977 <tanxc977@126.com>
2018-08-26 14:05:27 +0800

    ADD SPRING SECURITY SUPPORT

--- src/main/java/com/xc/mybook/MybookApplication.java
+++ src/main/java/com/xc/mybook/MybookApplication.java
@@ -3,44 +3,24 @@ package com.xc.mybook;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.web.servlet.MultipartConfigFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-import javax.servlet.MultipartConfigElement;
 
-@RestController
 @SpringBootApplication
-@Configuration
-@MapperScan(value = {"com.xc.auth.mapper","com.xc.mybook.Mapper"})
-public class MybookApplication {
+@MapperScan(value = {"com.xc.mybook.security.dao","com.xc.mybook.Mapper"})
+public class MybookApplication extends SpringBootServletInitializer {
 
-//	@RequestMapping("/")
-//	public String home(){
-//		return "hello home mybook !";
-//	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MybookApplication.class);
+	}
+
 
 	public static void main(String[] args) {
 		
 		SpringApplication.run(MybookApplication.class, args);
 	}
 
-	/**
-	 * 文件上传配置
-	 *
-	 * @return
-	 */
-	@Bean
-	public MultipartConfigElement multipartConfigElement() {
-		MultipartConfigFactory factory = new MultipartConfigFactory();
-		//  单个数据大小
-		factory.setMaxFileSize("200MB"); // KB,MB
-		/// 总上传数据大小
-		factory.setMaxRequestSize("200MB");
-		return factory.createMultipartConfig();
-	}
 
 }
